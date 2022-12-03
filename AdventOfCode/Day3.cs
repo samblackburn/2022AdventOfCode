a22d137dff9eb5ea9d284e426859a8aec73f638c
Sam Blackburn <sam.blackburn@red-gate.com>
2022-12-03 19:52:46 +0000

    Day 3 part 2

--- AdventOfCode/Day3.cs
+++ AdventOfCode/Day3.cs
@@ -20,6 +20,35 @@ public class Day3
         
         Console.WriteLine("Total: " + total);
     }
+    
+    [Test]
+    public void Part2()
+    {
+        var rucksacks = File.ReadAllLines("Day3Input.txt");
+        var total = 0;
+        var groups = 0;
+
+        var allItemTypes = File.ReadAllText("Day3Input.txt").ToHashSet();
+
+        for (var index = 0; index < rucksacks.Length - 2; index+=3)
+        {
+            var elf1 = rucksacks[index];
+            var elf2 = rucksacks[index + 1];
+            var elf3 = rucksacks[index + 2];
+
+            var common = elf1.ToHashSet()
+                .Intersect(elf2.ToHashSet())
+                .Intersect(elf3.ToHashSet());
+
+            var badgePriority = Priority(common.Single());
+            groups++;
+            total += badgePriority;
+        }
+        
+        Console.WriteLine("Total: " + total);
+        Console.WriteLine("Groups: " + groups);
+        Console.WriteLine("Rucksacks / 3: " + rucksacks.Length / 3);
+    }
 
     private int Priority(char item)
     {
