8122118a8c2b80dff0a622f68d43419540c16b83
Sam Blackburn <sam.blackburn@red-gate.com>
2022-12-03 19:04:23 +0000

    Day 3 part 1

---
+++ AdventOfCode/Day3.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode;
+
+[TestFixture]
+public class Day3
+{
+    [Test]
+    public void Part1()
+    {
+        var rucksacks = File.ReadAllLines("Day3Input.txt");
+        var total = 0;
+        foreach (var rucksack in rucksacks)
+        {
+            var totalSize = rucksack.Length;
+            var firstHalf = rucksack.Substring(0, totalSize / 2).ToHashSet();
+            var secondHalf = rucksack.Substring(totalSize / 2).ToHashSet();
+            var common = firstHalf.Intersect(secondHalf);
+            var priorities = common.Select(Priority).ToArray();
+            total += priorities.Sum();
+        }
+        
+        Console.WriteLine("Total: " + total);
+    }
+
+    private int Priority(char item)
+    {
+        if (char.IsUpper(item))
+        {
+            return 27 + item - 'A';
+        }
+        else
+        {
+            return 1 + item - 'a';
+        }
+    }
+}
