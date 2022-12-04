bb88cb4adbdc855872ad586fd273c39b98f6e064
Sam Blackburn <sam.blackburn@red-gate.com>
2022-12-04 14:20:45 +0000

    Day 4 part 2

--- AdventOfCode/Day4.cs
+++ AdventOfCode/Day4.cs
@@ -10,6 +10,23 @@ public class Day4
         var pairsWhereOneRangeFullyContainsTheOther = input.Where(OneRangeFullyContainsTheOther);
         Console.WriteLine(pairsWhereOneRangeFullyContainsTheOther.Count());
     }
+    
+    [Test]
+    public void Part2()
+    {
+        var input = File.ReadAllLines("Day4Input.txt");
+        var overlappingPairs = input.Where(Overlaps);
+        Console.WriteLine(overlappingPairs.Count());
+    }
+
+    private bool Overlaps(string line)
+    {
+        var parts = line.Split(',', '-').Select(int.Parse).ToArray();
+        var min = Math.Max(parts[0], parts[2]);
+        var max = Math.Min(parts[1], parts[3]);
+        //if (min <= max) Console.WriteLine($"{parts[0]}-{parts[1]} overlaps {parts[2]}-{parts[3]}");
+        return min <= max;
+    }
 
     private bool OneRangeFullyContainsTheOther(string line)
     {
