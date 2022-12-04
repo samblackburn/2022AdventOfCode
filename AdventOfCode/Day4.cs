d1bac082a2b3d6bfe83176daafdf117846e5692a
Sam Blackburn <sam.blackburn@red-gate.com>
2022-12-04 14:14:49 +0000

    Day 4 part 1 attempt 1

---
+++ AdventOfCode/Day4.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode;
+
+[TestFixture]
+public class Day4
+{
+    [Test]
+    public void Part1()
+    {
+        var input = File.ReadAllLines("Day4Input.txt");
+        var pairsWhereOneRangeFullyContainsTheOther = input.Where(OneRangeFullyContainsTheOther);
+        Console.WriteLine(pairsWhereOneRangeFullyContainsTheOther.Count());
+    }
+
+    private bool OneRangeFullyContainsTheOther(string line)
+    {
+        var parts = line.Split(',', '-').Select(int.Parse).ToArray();
+        var firstContainsSecond = parts[0] >= parts[3] && parts[2] <= parts[4];
+        var secondContainsFirst = parts[0] <= parts[3] && parts[2] >= parts[4];
+        return firstContainsSecond || secondContainsFirst;
+    }
+}
