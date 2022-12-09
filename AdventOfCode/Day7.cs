d9f8441b3c31feb88f888cd9c65d4eecc207ec29
Sam Blackburn <sam.blackburn@red-gate.com>
2022-12-09 15:38:11 +0000

    Day 7 part 1: Ensure a key exists for directory that only contains subdirectories
    (cherry picked from commit 4036344f9624b71a282405e23f4ed7c5acaeb58d)
    
    # Conflicts:
    #	AdventOfCode/Day7.cs

--- AdventOfCode/Day7.cs
+++ AdventOfCode/Day7.cs
@@ -4,12 +4,12 @@
 public class Day7
 {
     private string m_Path;
-    private IDictionary<string, long> m_FileSizes;
+    private IDictionary<string, long> m_DirectorySizes;
 
     private static IEnumerable<TestCaseData> Day7Inputs => new[]
     {
         new TestCaseData(new object[] {File.ReadAllLines("Day7Input.txt")})
-            {ExpectedResult = 1331842, TestName = "Day 7 input"},
+            {ExpectedResult = 1501149, TestName = "Day 7 input"},
         new TestCaseData(new object[]
         {
             @"$ cd /
@@ -47,14 +47,14 @@ dir b
 $ cd b
 $ ls
 584 c.txt".Split(Environment.NewLine)
-        }) {ExpectedResult = 584 * 2, TestName = "A folder that only contains a subfolder"},
+        }) {ExpectedResult = 584 * 3, TestName = "A folder that only contains a subfolder"},
     };
 
     [TestCaseSource(nameof(Day7Inputs))]
     public long Part1(string[] lines)
     {
         m_Path = ":";
-        m_FileSizes = new Dictionary<string, long>();
+        m_DirectorySizes = new Dictionary<string, long> {{":", 0}};
 
         foreach (var line in lines)
         {
@@ -68,22 +68,18 @@ $ ls
             }
         }
 
-        foreach (var kvp in m_FileSizes)
+        foreach (var kvp in m_DirectorySizes)
         {
             //Console.WriteLine($"{kvp.Key} is of size {kvp.Value}");
         }
 
-        var allDirs = m_FileSizes
-            .GroupBy(f => Parent(f.Key))
-            .Select(g => g.Key)
-            .ToArray();
         var total = 0L;
-        foreach (var dir in allDirs)
+        foreach (var dir in m_DirectorySizes)
         {
-            var size = SizeWithDescendents(dir);
+            var size = SizeWithDescendents(dir.Key);
             if (size > 100_000L) continue;
             total += size;
-            Console.WriteLine($"Folder {dir} is of size {size}");
+            Console.WriteLine($"Folder {dir.Key} is of size {size}");
         }
         
         Console.WriteLine(total);
@@ -92,8 +88,8 @@ $ ls
 
     private long SizeWithDescendents(string dir)
     {
-        return m_FileSizes
-            .Where(kvp => kvp.Key.StartsWith(dir + '/'))
+        return m_DirectorySizes
+            .Where(kvp => kvp.Key.StartsWith(dir))
             .Sum(kvp => kvp.Value);
     }
 
@@ -102,7 +98,7 @@ $ ls
         var parts = line.Split(" ");
         if (parts[0] == "dir")
         {
-            
+            m_DirectorySizes.Add(m_Path + "/" + parts[1], 0);
         }
         else if (!long.TryParse(parts[0], out var size))
         {
@@ -110,7 +106,7 @@ $ ls
         }
         else
         {
-            m_FileSizes.Add(m_Path + "/" + parts[1], size);
+            m_DirectorySizes[m_Path] += size;
         }
     }
 
