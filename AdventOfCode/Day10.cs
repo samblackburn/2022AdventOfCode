09e55613bf647c48ce0ef4d4a8871e6d4fdd4369
Sam Blackburn <sam.blackburn@red-gate.com>
2022-12-12 13:30:14 +0000

    Day 10 part 2

--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -36,6 +36,35 @@ public class Day10
         return m_Result;
     }
     
+    [TestCase("Day10ExampleInput.txt")]
+    [TestCase("Day10Input.txt")]
+    public void Part2(string fileName)
+    {
+        var input = File.ReadAllLines(fileName);
+        foreach (var line in input)
+        {
+            if (line == "noop")
+            {
+                IncrementPart2();
+            }
+            else
+            {
+                IncrementPart2();
+                IncrementPart2();
+                m_Register += int.Parse(line.Split(" ")[1]);
+            }
+        }
+    }
+
+    private void IncrementPart2()
+    {
+        m_Clock++;
+        var pixel = m_Clock % 40;
+        var sprite = m_Register + 1;
+        Console.Write(Math.Abs(pixel - sprite) < 2 ? "#" : ".");
+
+        if (pixel % 40 == 0) Console.WriteLine();
+    }
 
     private void Increment()
     {
