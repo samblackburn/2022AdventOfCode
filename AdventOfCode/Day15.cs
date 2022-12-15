7ef0300065bd76a9beb5f56ec329aad2682ed2f6
Sam Blackburn <sam.blackburn@red-gate.com>
2022-12-15 13:06:28 +0000

    Day 15 part 2. Takes 3min11s!

--- AdventOfCode/Day15.cs
+++ AdventOfCode/Day15.cs
@@ -1,13 +1,13 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Text.RegularExpressions;
 using System.Xml.Xsl;
 
 namespace AdventOfCode;
 
 public class Day15
 {
-    [TestCase(@"Sensor at x=0, y=0: closest beacon is at x=1, y=1", 0, ExpectedResult = 5)]
-    [TestCase(@"Sensor at x=9, y=16: closest beacon is at x=10, y=16", 10, ExpectedResult = 0)]
-    [TestCase(@"Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+    private const string ExampleInput = @"Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
 Sensor at x=13, y=2: closest beacon is at x=15, y=3
 Sensor at x=12, y=14: closest beacon is at x=10, y=16
@@ -20,7 +20,11 @@ Sensor at x=20, y=14: closest beacon is at x=25, y=17
 Sensor at x=17, y=20: closest beacon is at x=21, y=22
 Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3", 10, ExpectedResult = 26, TestName = "Example input")]
+Sensor at x=20, y=1: closest beacon is at x=15, y=3";
+
+    [TestCase(@"Sensor at x=0, y=0: closest beacon is at x=1, y=1", 0, ExpectedResult = 5)]
+    [TestCase(@"Sensor at x=9, y=16: closest beacon is at x=10, y=16", 10, ExpectedResult = 0)]
+    [TestCase(ExampleInput, 10, ExpectedResult = 26, TestName = "Example input")]
     [TestCase(null, 2000000, ExpectedResult = 5564017, TestName = "Challenge input")]
     public int Part1(string? input, int y)
     {
@@ -43,6 +47,46 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3", 10, ExpectedResult = 26, T
         var beaconsOnLine = sensors.Where(x => x.beaconY == y).Select(x => x.beaconX).Distinct().Count();
 
         return line.Count(isCovered => isCovered) - beaconsOnLine;
+    }
+
+    [TestCase(ExampleInput, 20, ExpectedResult = 56000011, TestName = "Example input")]
+    [TestCase(@"Sensor at x=0, y=0: closest beacon is at x=1, y=1", 2, ExpectedResult = -1)]
+    [TestCase(@"Sensor at x=0, y=0: closest beacon is at x=1, y=1", 3, ExpectedResult = 8000001)]
+    [TestCase(null, 4000000, ExpectedResult = 11558423398893, Explicit = true)]
+    public long Part2(string? input, int maxCoord)
+    {
+        input ??= File.ReadAllText("Day15Input.txt");
+        var sensors = input.Split(Environment.NewLine).Select(Parse).ToList();
+
+        var approxLocations = new HashSet<Point>();
+
+        var boxSize = Math.Min(200, maxCoord);
+        for (var y = boxSize/2; y < maxCoord; y+=boxSize)
+        {
+            for (var x = boxSize/2; x < maxCoord; x+=100)
+            {
+                if (sensors.Any(s => Math.Abs(x - s.X) + Math.Abs(y - s.Y) <= s.Distance - boxSize)) continue;
+                approxLocations.Add(new Point(x, y));
+                //Console.WriteLine($"Possible match around ({x}, {y})");
+            }
+        }
+
+        Console.WriteLine($"Found {approxLocations.Count} approx locations");
+
+        foreach (var approxLocation in approxLocations)
+        {
+            for (var y = approxLocation.Y - boxSize/2; y < approxLocation.Y + (boxSize + 1)/2; y++)
+            {
+                for (var x = approxLocation.X - boxSize/2; x < approxLocation.X + (boxSize + 1)/2; x++)
+                {
+                    if (sensors.Any(s => s.beaconX == x && s.beaconY == y)) continue;
+                    if (sensors.Any(s => Math.Abs(x - s.X) + Math.Abs(y - s.Y) <= s.Distance)) continue;
+                    return (long) x * 4000000 + y;
+                }
+            }
+        }
+
+        return -1;
     }
 
     private Sensor Parse(string line)
