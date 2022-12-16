f43326ebc4934b5f992dfc53760d3ee46ae0f98c
Sam Blackburn <sam.blackburn@red-gate.com>
2022-12-16 19:12:04 +0000

    Day 16 part 2 attempt 1.

--- AdventOfCode/Day16.cs
+++ AdventOfCode/Day16.cs
@@ -10,7 +10,21 @@ public class Day16
         // Guess computed the sad way, by drawing the graph on a piece of paper
         // and tracing a path by hand
         var myGuess = @"XQ VP VM TR VM DO KI HN".Split(" ").ToList();
-        Simulate(myGuess, nodes);
+        Simulate(myGuess, nodes, 30);
+    }
+    
+    [Test]
+    public void Part2()
+    {
+        var nodes = Parse();
+
+        // Guess computed the even sadder way, with a lot of trial and error
+        var myGuess = @"VP VM TR !VM DO KI HN !KI !DO".Split(" ").ToList();
+        var elephantGuess = @"XQ GA !XQ SH QH VW !QH !MN".Split(" ").ToList();
+
+        var total = Simulate(myGuess, nodes, 26) + Simulate(elephantGuess, nodes, 26);
+        Assert.Greater(total, 2285);
+        Console.WriteLine(total);
     }
 
     private Dictionary<string, Valve> Parse()
@@ -21,7 +35,7 @@ public class Day16
 
         foreach (var node in nodes.Values.Where(IsNonTrivial).OrderByDescending(x => x.Tunnels.Count()))
         {
-            Console.Write($"Valve {node.Id} has flow rate={node.FlowRate}; tunnels lead to valves ");
+            //Console.Write($"Valve {node.Id} has flow rate={node.FlowRate}; tunnels lead to valves ");
 
             foreach (var tunnel in node.Tunnels)
             {
@@ -37,49 +51,50 @@ public class Day16
                 }
 
                 node.UsefulTunnels.Add((distance, thisTunnel));
-                Console.Write($"{thisTunnel}@{distance} ");
+                //Console.Write($"{thisTunnel}@{distance} ");
             }
 
-            Console.WriteLine();
+            //Console.WriteLine();
         }
 
         return nodes;
     }
 
-    private static void Simulate(List<string> myGuess, Dictionary<string, Valve> nodes)
+    private static int Simulate(List<string> myGuess, Dictionary<string, Valve> nodes, int timeLimit)
     {
         var opened = new HashSet<string>();
         var time = 0;
         var released = 0;
         var releaseRate = 0;
         var prevNode = "AA";
-        foreach (var node in myGuess)
+        foreach (var nodeWithBang in myGuess)
         {
+            var node = nodeWithBang.Trim('!');
             var distance = nodes[node].UsefulTunnels.Single(t => t.destination == prevNode).distance;
-            if (time + distance > 30) break;
+            if (time + distance > timeLimit) break;
             time += distance;
             released += distance * releaseRate;
-            Console.WriteLine(
-                $"t={time} Moved from {prevNode} to {node}. Valves {string.Join(", ", opened)} are open, releasing {releaseRate}/min for {released} total pressure.");
+            Console.WriteLine($"t={time} Moved {distance} from {prevNode} to {node}. Valves {string.Join(", ", opened)} are open, releasing {releaseRate}/min for {released} total pressure.");
             prevNode = node;
 
-            if (!opened.Contains(node))
+            if (!opened.Contains(node) && !nodeWithBang.Contains('!'))
             {
                 time++;
                 released += releaseRate;
                 opened.Add(node);
                 releaseRate += nodes[node].FlowRate;
-                Console.WriteLine($"t={time} Opened valve {node}");
+                Console.WriteLine($"t={time} Opened valve {nodeWithBang}");
             }
             else
             {
-                Console.WriteLine($"t={time} Valve {node}already open.");
+                Console.WriteLine($"t={time} Valve {nodeWithBang} already open.");
             }
         }
 
-        var remainingTime = 30 - time;
+        var remainingTime = timeLimit - time;
         released += releaseRate * remainingTime;
-        Console.WriteLine($"t=30 Time up! Total released: {released}");
+        Console.WriteLine($"t={timeLimit} Time up! Total released: {released}");
+        return released;
     }
 
     private bool IsNonTrivial(Valve valve) => valve.FlowRate > 0 || valve.Tunnels.Count() > 2;
