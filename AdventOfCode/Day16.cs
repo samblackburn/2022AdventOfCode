f078e995fc860eda85566e53629522d0cf1dc50a
Sam Blackburn <sam.blackburn@red-gate.com>
2022-12-16 17:17:48 +0000

    Extract method

--- AdventOfCode/Day16.cs
+++ AdventOfCode/Day16.cs
@@ -5,6 +5,16 @@ public class Day16
     [Test]
     public void Part1()
     {
+        var nodes = Parse();
+
+        // Guess computed the sad way, by drawing the graph on a piece of paper
+        // and tracing a path by hand
+        var myGuess = @"XQ VP VM TR VM DO KI HN".Split(" ").ToList();
+        Simulate(myGuess, nodes);
+    }
+
+    private Dictionary<string, Valve> Parse()
+    {
         var input = File.ReadAllText("Day16Input.txt");
 
         var nodes = input.Split(Environment.NewLine).Select(line => new Valve(line)).ToDictionary(n => n.Id);
@@ -12,7 +22,7 @@ public class Day16
         foreach (var node in nodes.Values.Where(IsNonTrivial).OrderByDescending(x => x.Tunnels.Count()))
         {
             Console.Write($"Valve {node.Id} has flow rate={node.FlowRate}; tunnels lead to valves ");
-            
+
             foreach (var tunnel in node.Tunnels)
             {
                 var prevTunnel = node.Id;
@@ -29,13 +39,15 @@ public class Day16
                 node.UsefulTunnels.Add((distance, thisTunnel));
                 Console.Write($"{thisTunnel}@{distance} ");
             }
-            
+
             Console.WriteLine();
         }
 
-        // Guess computed the sad way, by drawing the graph on a piece of paper
-        // and tracing a path by hand
-        var myGuess = @"XQ VP VM TR VM DO KI HN".Split(" ").ToList();
+        return nodes;
+    }
+
+    private static void Simulate(List<string> myGuess, Dictionary<string, Valve> nodes)
+    {
         var opened = new HashSet<string>();
         var time = 0;
         var released = 0;
@@ -47,7 +59,8 @@ public class Day16
             if (time + distance > 30) break;
             time += distance;
             released += distance * releaseRate;
-            Console.WriteLine($"t={time} Moved from {prevNode} to {node}. Valves {string.Join(", ", opened)} are open, releasing {releaseRate}/min for {released} total pressure.");
+            Console.WriteLine(
+                $"t={time} Moved from {prevNode} to {node}. Valves {string.Join(", ", opened)} are open, releasing {releaseRate}/min for {released} total pressure.");
             prevNode = node;
 
             if (!opened.Contains(node))
